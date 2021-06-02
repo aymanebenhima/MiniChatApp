c98550b25bcb5de98661b83623dcd8beb1d4c8d9
Aymane Benhima <aymanebenhima@gmail.com>
2021-06-02 01:42:10 +0100

    storing message within chat component into db

--- app/Http/Livewire/Chat.php
+++ app/Http/Livewire/Chat.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Message;
 use Livewire\Component;
 
 class Chat extends Component
 {
+
+    public $messageText;
+
     public function render()
     {
-        return view('livewire.chat');
+        $messages = Message::with('user')->latest()->take(10)->get()->sortBy('id');
+
+        return view('livewire.chat', compact('messages'));
     }
+
+    public function sendMessage()
+    {
+        Message::create([
+            'user_id' => auth()->user()->id,
+            'message_text' => $this->messageText,
+        ]);
+
+        $this->reset('messageText');
+    }
+
 }
