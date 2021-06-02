d5dc9a9eb126c85b21ab3380c0af96a88a7dda04
Aymane Benhima <aymanebenhima@gmail.com>
2021-06-02 01:43:05 +0100

    styling and viewing chat component

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,3 +1,60 @@
 <div wire:poll>
+    <div class="container">
+        <h3 class=" text-center">
 
+            @if (auth()->user()->email == 'aymanebenhima@gmail.com')
+                <a class="btn btn-primary" href="{{ Url('delete_chat') }}">Delete conversation</a>
+            @endif
+
+        </h3>
+        <div class="messaging">
+            <div class="inbox_msg">
+                <div class="mesgs">
+                    <div id="chat" class="msg_history">
+                        @forelse ($messages as $message)
+
+                            @if ($message->user->name == auth()->user()->name)
+                                <!-- Reciever Message-->
+                                <div class="outgoing_msg">
+                                    <div class="sent_msg">
+                                        <p>{{ $message->message_text }}</p>
+                                        <span class="time_date">
+                                            {{ $message->created_at->diffForHumans(null, false, false) }}</span>
+                                    </div>
+                                </div>
+
+                            @else
+                                <div class="incoming_msg">{{ $message->user->name }}
+                                    <div class="incoming_msg_img"> <img
+                                            src="https://www.shareicon.net/data/64x64/2016/09/01/822711_user_512x512.png" alt="sunil"> </div>
+                                    <div class="received_msg">
+                                        <div class="received_withd_msg">
+                                            <p>{{ $message->message_text }}</p>
+                                            <span
+                                                class="time_date">{{ $message->created_at->diffForHumans(null, false, false) }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            @endif
+                        @empty
+                            <h5 style="text-align: center;color:red"> There is no message!</h5>
+                        @endforelse
+
+                    </div>
+                    <div class="type_msg">
+                        <div class="input_msg_write">
+                            <form wire:submit.prevent="sendMessage">
+                                <input onkeydown='scrollDown()' wire:model.defer="messageText" type="text"
+                                    class="write_msg" placeholder="write your message" />
+                                <button class="msg_send_btn" type="submit">Send</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
